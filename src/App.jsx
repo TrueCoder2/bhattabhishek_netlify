9d2c42d0d8b53da79afb2408d7ca881a3d1ea843
Abhishek Bhatt <121381930+TrueCoder2@users.noreply.github.com>
2023-12-04 08:37:54 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      Hi there , 🖐 <br/>
+      <h1>Abhishek Bhatt</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
